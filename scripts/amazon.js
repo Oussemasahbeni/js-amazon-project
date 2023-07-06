f23f6a0148d972426c123a105392d7ee579b1923
Oussema Sahbeni <104161749+Oussemasahbeni@users.noreply.github.com>
2023-07-07 00:24:40 +0100

    use products.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,38 +1,49 @@
 // an array represet a list
 // this is called a data structure
-const products= [{
-    image:'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name:'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating:{
-        count:87,
-        stars:4.5
-    },
-    priceCents:1090,  // we save price in cents cause floats sometimes can not be precise
+// const products= [{
+//     image:'images/products/athletic-cotton-socks-6-pairs.jpg',
+//     name:'Black and Gray Athletic Cotton Socks - 6 Pairs',
+//     rating:{
+//         count:87,
+//         stars:4.5
+//     },
+//     priceCents:1090,  // we save price in cents cause floats sometimes can not be precise
 
 
-},
-{
-    image:'images/products/intermediate-composite-basketball.jpg',
-    name:' Intermediate Size Basketball',
-    rating:{
-        count:127,
-        stars:4
-    },
-    priceCents:2095,  
+// },
+// {
+//     image:'images/products/intermediate-composite-basketball.jpg',
+//     name:' Intermediate Size Basketball',
+//     rating:{
+//         count:127,
+//         stars:4
+//     },
+//     priceCents:2095,  
 
-},
-{
-    image:'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name:' Adults Plain Cotton T-Shirt - 2 Pack',
-    rating:{
-        count:56,
-        stars:4.5
-    },
-    priceCents:799,  
+// },
+// {
+//     image:'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+//     name:' Adults Plain Cotton T-Shirt - 2 Pack',
+//     rating:{
+//         count:56,
+//         stars:4.5
+//     },
+//     priceCents:799,  
 
-}
-]
+// },
+// {
+//     image:'images/products/black-2-slot-toaster.jpg',
+//     name:' 2 slot toaster-Black',
+//     rating:{
+//         count:2197,
+//         stars:5
+//     },
+//     priceCents:1899
+// }
+// ]
 
+
+// products coming from products.js file
 let productsHtml='';
 products.forEach((product)=>
 {
